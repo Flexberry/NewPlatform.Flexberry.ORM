593e27448bc03fc399a9f6cb6f37786f55705659
Bratchikov Igor <bratchikov@ics.perm.ru>
2018-02-14 15:28:38 +0500

    Try to fix broken unit test

--- NewPlatform.Flexberry.ORM.Tests/Properties/AssemblyInfo.cs
+++ NewPlatform.Flexberry.ORM.Tests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("New Platform Ltd.")]
 [assembly: AssemblyProduct("Flexberry ORM")]
-[assembly: AssemblyCopyright("Copyright © New Platform Ltd. 2015")]
+[assembly: AssemblyCopyright("Copyright © New Platform Ltd. 2018")]
 [assembly: AssemblyTrademark("Flexberry")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: Xunit.CollectionBehavior(DisableTestParallelization = true)]
