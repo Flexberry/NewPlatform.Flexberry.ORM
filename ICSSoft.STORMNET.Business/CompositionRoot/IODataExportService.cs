2178a155d5fe8680ef733a6be59b5e27b8457b91
akosinsky <akosinsky@ICS-KK12324>
2018-03-30 16:28:55 +0500

    Add interface IODataExportService

---
+++ ICSSoft.STORMNET.Business/CompositionRoot/IODataExportService.cs
@@ -0,0 +1,27 @@
+﻿namespace NewPlatform.Flexberry
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using ICSSoft.STORMNET;
+    using ICSSoft.STORMNET.Business;
+    using System.Collections.Specialized;
+
+    /// <summary>
+    /// Сервис экспорта данных из ODataService.
+    /// </summary>
+    public interface IODataExportService
+    {
+        /// <summary>
+        /// Создаёт файл экспорта  данных из ODataService.
+        /// </summary>
+        /// <param name="dataService">Сервис данных ORM.</param>
+        /// <param name="parameters">Параметры экпорта.</param>
+        /// <param name="objs">Объекты для экспорта.</param>
+        /// <param name="queryParams">Параметры в строке запроса к ODataService.</param>
+        /// <returns>Возвращает файл экспорта в виде MemoryStream.</returns>
+        MemoryStream CreateExportStream(IDataService dataService, IExportParams parameters, DataObject[] objs, NameValueCollection queryParams);
+    }
+}
