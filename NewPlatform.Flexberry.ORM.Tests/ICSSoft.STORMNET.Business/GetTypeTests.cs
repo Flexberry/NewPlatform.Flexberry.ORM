86bfc8d6534e5bed14a91ff2db157850c801bdd3
Lekomtsev Denis <dlekomtsev@ics.perm.ru>
2019-08-14 15:21:11 +0500

    fix ICSSoft.STORMNET.Business project version

---
+++ NewPlatform.Flexberry.ORM.Tests/ICSSoft.STORMNET.Business/GetTypeTests.cs
@@ -0,0 +1,17 @@
+﻿namespace NewPlatform.Flexberry.ORM.Tests
+{
+    using System;
+
+    using Xunit;
+
+    public class GetTypeTests
+    {
+        [Fact]
+        public void GetIDataServiceTypeTest()
+        {
+            string typeNameForIDataService = "ICSSoft.STORMNET.Business.IDataService, ICSSoft.STORMNET.Business, Version=1.0.0.1, Culture=neutral, PublicKeyToken=c17bb360f7843f45";
+            Type iDataServiceType = Type.GetType(typeNameForIDataService);
+            Assert.NotNull(iDataServiceType);
+        }
+    }
+}
