024f3b9c445cfdb077fc0345bd4f02d13aabeda5
NicholasNoise <36186214+NicholasNoise@users.noreply.github.com>
2020-09-04 19:16:49 +0500

    Convert test project to Microsoft.NET.Sdk. (#138)
    * Convert test project to Microsoft.NET.Sdk.
    Initial support netcoreapp3.1.
    
    * Using dotnet test for travis.
    
    * Fix class name typo.
    
    Co-authored-by: Tsar Nikolay <nsmirnov@ics.perm.ru>

--- NewPlatform.Flexberry.ORM.IntegratedTests/ICSSoft.STORMNET.FunctionalLanguage/LoadingCustomizationStructTest.cs
+++ NewPlatform.Flexberry.ORM.IntegratedTests/ICSSoft.STORMNET.FunctionalLanguage/LoadingCustomizationStructTest.cs
@@ -1,46 +1,51 @@
 ﻿namespace ICSSoft.STORMNET.Tests.TestClasses.FunctionalLanguage
 {
+#if !NETCORE
     using System;
     using ICSSoft.STORMNET.Business;
     using ICSSoft.STORMNET.FunctionalLanguage;
     using ICSSoft.STORMNET.Tools;
-    using Xunit;
+
     using NewPlatform.Flexberry.ORM.Tests;
+
+    using Xunit;
+#endif
 
     /// <summary>
     /// TODO: Update summary.
     /// </summary>
-    
     public class LoadingCustomizationStructTest
     {
         private STORMNET.FunctionalLanguage.SQLWhere.SQLWhereLanguageDef _ldef =
             STORMNET.FunctionalLanguage.SQLWhere.SQLWhereLanguageDef.LanguageDef;
+
+#if !NETCORE
         [Fact]
-        
         public void LcsSerializationTest()
         {
             // TODO: починить тест
             var lcs = new LoadingCustomizationStruct(1)
-                {
-                    ColumnsSort = new[] { new ColumnsSortDef("Колонка1", SortOrder.Desc) },
-                    LimitFunction =
-                        _ldef.GetFunction(_ldef.funcEQ, new VariableDef(_ldef.GuidType, "STORMMainObjectKey"), Guid.NewGuid().ToString()),
-                    LoadingTypes = new[] { typeof(DataObjectForTest) },
-                    View = new View(typeof(DataObjectForTest), View.ReadType.OnlyThatObject),
-                    ColumnsOrder = new[] { "Фамилия", "Name" },
-                    AdvansedColumns =
-                        new[] { new AdvansedColumn("Колонка", "myExpression", "myStorageSourceModification") },
-                    InitDataCopy = true,
-                    ReturnTop = 100,
-                    LoadingBufferSize = 1000,
-                    RowNumber = new RowNumberDef(9, 89),
-                    Distinct = true,
-                    ReturnType = LcsReturnType.Object
-                };
+            {
+                ColumnsSort = new[] { new ColumnsSortDef("Колонка1", SortOrder.Desc) },
+                LimitFunction =
+                    _ldef.GetFunction(_ldef.funcEQ, new VariableDef(_ldef.GuidType, "STORMMainObjectKey"), Guid.NewGuid().ToString()),
+                LoadingTypes = new[] { typeof(DataObjectForTest) },
+                View = new View(typeof(DataObjectForTest), View.ReadType.OnlyThatObject),
+                ColumnsOrder = new[] { "Фамилия", "Name" },
+                AdvansedColumns =
+                    new[] { new AdvansedColumn("Колонка", "myExpression", "myStorageSourceModification") },
+                InitDataCopy = true,
+                ReturnTop = 100,
+                LoadingBufferSize = 1000,
+                RowNumber = new RowNumberDef(9, 89),
+                Distinct = true,
+                ReturnType = LcsReturnType.Object
+            };
 
             var lcsStr = ToolBinarySerializer.ObjectToString(lcs);
             var newLcs = (LoadingCustomizationStruct)ToolBinarySerializer.ObjectFromString(lcsStr);
             Assert.True(newLcs.Equals(lcs));
         }
+#endif
     }
 }
