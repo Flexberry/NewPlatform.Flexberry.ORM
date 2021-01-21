c1be80d06fc78476728daaff551917ab795d2067
ics_home\ananin <anton-iv87@yandex.ru>
2021-01-21 13:38:54 +0500

    Change interface name and fixes

--- ICSSoft.STORMNET.Business/CompositionRoot/IExportStringedObjectViewService.cs
+++ ICSSoft.STORMNET.Business/CompositionRoot/IExportStringedObjectViewService.cs
@@ -4,9 +4,9 @@
     using ICSSoft.STORMNET.Business;
 
     /// <summary>
-    /// Data export service with ObjectStringDataView .
+    /// Data export service with ObjectStringDataView.
     /// </summary>
-    public interface IODataExportServiceFast
+    public interface IExportStringedObjectViewService
     {
         /// <summary>
         /// Creates a file for exporting data from ODataService.
@@ -14,6 +14,6 @@
         /// <param name="parameters">Export options.</param>
         /// <param name="objs">Objects for export.</param>
         /// <returns>Returns the export file as MemoryStream.</returns>
-        MemoryStream CreateExportStreamFast(IExportParams parameters, ObjectStringDataView[] objs);
+        MemoryStream CreateExportStream(IExportParams parameters, ObjectStringDataView[] objs);
     }
 }
