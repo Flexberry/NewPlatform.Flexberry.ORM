83ed0952040b48fa2aec0557da5bcc09d4f1a2bf
Ilya Naydanov <turbcool@gmail.com>
2022-03-02 12:55:51 +0500

    UPD async data service draft

--- ICSSoft.STORMNET.Business/IAsyncDataService.cs
+++ ICSSoft.STORMNET.Business/IAsyncDataService.cs
@@ -3,28 +3,14 @@ namespace ICSSoft.STORMNET.Business
     using System;
     using System.Collections.Generic;
     using System.Threading.Tasks;
-
+    using ICSSoft.STORMNET;
+    using ICSSoft.STORMNET.Business;
     using ICSSoft.STORMNET.Business.Audit;
     using ICSSoft.STORMNET.Security;
 
     public interface IAsyncDataService : ICloneable
     {
-        DataServiceOptions Options { get; set; }
-
-        /// <summary>
-        /// Менеджер полномочий.
-        /// </summary>
-        ISecurityManager SecurityManager { get; }
-
-        /// <summary>
-        /// Текущий сервис аудита.
-        /// </summary>
-        IAuditService AuditService { get; }
-
-        /// <summary>
-        /// Сервис кэша.
-        /// </summary>
-        ICacheService CacheService { get; }
+        //Нужен ли? DataServiceOptions Options { get; set; }
 
         /// <summary>
         /// Возвращает количество объектов удовлетворяющих запросу.
@@ -34,35 +20,35 @@ namespace ICSSoft.STORMNET.Business
         /// Объект <see cref="Task"/>, представляющий асинхронную операцию.
         /// Результат содержит количество объектов.
         /// </returns>
-        Task<int> GetObjectsCount(LoadingCustomizationStruct customizationStruct);
+        Task<int> GetObjectsCountAsync(LoadingCustomizationStruct customizationStruct);
 
         /// <summary>
         /// Догрузка одного объекта данных.
         /// </summary>
         /// <param name="dobject">Объект данных, который требуется загрузить.</param>
         /// <param name="clearDataObject">Очищать ли объект.</param>
-        /// <param name="checkExistingObject">Проверять ли существование объекта в хранилище.</param>
+        /// <param name="checkExistingObject">Проверять ли существование объекта в хранилище (вызывать исключение если объекта нет).</param>
         /// <returns>Объект <see cref="Task"/>, представляющий асинхронную операцию.</returns>
-        Task LoadObject(DataObject dobject, bool clearDataObject = true, bool checkExistingObject = true);
+        Task LoadObjectAsync(DataObject dobject, bool clearDataObject = true, bool checkExistingObject = true);
 
         /// <summary>
         /// Догрузка одного объекта данных по представлению.
         /// </summary>
         /// <param name="dobject">Объект данных, который требуется загрузить.</param>
         /// <param name="dataObjectView">Представление.</param>
-        /// <param name="clearDataObject">Очищать ли объект.</param>
-        /// <param name="checkExistingObject">Проверять ли существование объекта в хранилище.</param>
+        /// <param name="clearDataObject">Очищать загруженные объекты (см. <see cref="ICSSoft.STORMNET.DataObject.Clear"/>).</param>
+        /// <param name="checkExistingObject">Проверять ли существование объекта в хранилище (вызывать исключение если объекта нет).</param>
         /// <returns>Объект <see cref="Task"/>, представляющий асинхронную операцию.</returns>
-        Task LoadObject(DataObject dobject, View dataObjectView, bool clearDataObject = true, bool checkExistingObject = true);
+        Task LoadObjectAsync(DataObject dobject, View dataObjectView, bool clearDataObject = true, bool checkExistingObject = true);
 
         /// <summary>
         /// Догрузка объектов данных по представлению.
         /// </summary>
         /// <param name="dataobjects">Исходные объекты.</param>
         /// <param name="dataObjectView">Представлене.</param>
-        /// <param name="clearDataobject">Очищать ли существующие.</param>
+        /// <param name="clearDataobject">Очищать загруженные объекты (см. <see cref="ICSSoft.STORMNET.DataObject.Clear"/>).</param>
         /// <returns>Объект <see cref="Task"/>, представляющий асинхронную операцию.</returns>
-        Task LoadObjects(IEnumerable<DataObject> dataobjects, View dataObjectView, bool clearDataobject = true);
+        Task LoadObjectsAsync(IEnumerable<DataObject> dataobjects, View dataObjectView, bool clearDataobject = true);
 
         /// <summary>
         /// Загрузка объектов данных по представлению.
@@ -72,7 +58,7 @@ namespace ICSSoft.STORMNET.Business
         /// Объект <see cref="Task"/>, представляющий асинхронную операцию.
         /// Результат содержит коллекцию объектов данных.
         /// </returns>
-        Task<IEnumerable<DataObject>> LoadObjects(View dataObjectView);
+        Task<IEnumerable<DataObject>> LoadObjectsAsync(View dataObjectView);
 
         /// <summary>
         /// Загрузка объектов данных по нескольким представлениям.
@@ -82,7 +68,7 @@ namespace ICSSoft.STORMNET.Business
         /// Объект <see cref="Task"/>, представляющий асинхронную операцию.
         /// Результат содержит коллекцию объектов данных.
         /// </returns>
-        Task<IEnumerable<DataObject>> LoadObjects(IEnumerable<View> dataObjectViews);
+        Task<IEnumerable<DataObject>> LoadObjectsAsync(IEnumerable<View> dataObjectViews);
 
         /// <summary>
         /// Загрузка объектов данных по массиву структур.
@@ -92,87 +78,84 @@ namespace ICSSoft.STORMNET.Business
         /// Объект <see cref="Task"/>, представляющий асинхронную операцию.
         /// Результат содержит коллекцию объектов данных.
         /// </returns>
-        Task<IEnumerable<DataObject>> LoadObjects(IEnumerable<LoadingCustomizationStruct> customizationStructs);
+        Task<IEnumerable<DataObject>> LoadObjectsAsync(IEnumerable<LoadingCustomizationStruct> customizationStructs);
 
         /// <summary>
         /// Загрузка объектов данных по представлению.
         /// </summary>
-        /// <param name="dataObjectView">представление.</param>
-        /// <param name="changeViewForTypeDelegate">делегат для изменения.</param>
+        /// <param name="dataObjectView">Представление.</param>
+        /// <param name="changeViewForTypeDelegate">Делегат, возвращающий представление в зависимости от типа объекта (используется когда получаемый список DataObject'ов может состоять из нескольких типов - напр. при наследовании DataObject'ов).</param>
         /// <returns>
         /// Объект <see cref="Task"/>, представляющий асинхронную операцию.
         /// Результат содержит коллекцию объектов данных.
         /// </returns>
-        Task<IEnumerable<DataObject>> LoadObjects(View dataObjectView, ChangeViewForTypeDelegate changeViewForTypeDelegate);
+        Task<IEnumerable<DataObject>> LoadObjectsAsync(View dataObjectView, ChangeViewForTypeDelegate changeViewForTypeDelegate);
 
         /// <summary>
         /// Загрузка объектов данных по массиву представлений.
         /// </summary>
-        /// <param name="dataObjectViews">массив представлений.</param>
-        /// <param name="changeViewForTypeDelegate">делегат для изменения.</param>
+        /// <param name="dataObjectViews">Массив представлений.</param>
+        /// <param name="changeViewForTypeDelegate">Делегат, возвращающий представление в зависимости от типа объекта (используется когда получаемый список DataObject'ов может состоять из нескольких типов - напр. при наследовании DataObject'ов).</param>
         /// <returns>
         /// Объект <see cref="Task"/>, представляющий асинхронную операцию.
         /// Результат содержит коллекцию объектов данных.
         /// </returns>
-        Task<IEnumerable<DataObject>> LoadObjects(IEnumerable<View> dataObjectViews, ChangeViewForTypeDelegate changeViewForTypeDelegate);
+        Task<IEnumerable<DataObject>> LoadObjectsAsync(IEnumerable<View> dataObjectViews, ChangeViewForTypeDelegate changeViewForTypeDelegate);
 
         /// <summary>
         /// Загрузка объектов данных по массиву структур.
         /// </summary>
-        /// <param name="customizationStructs">массив структур.</param>
-        /// <param name="changeViewForTypeDelegate">делегат для изменения.</param>
+        /// <param name="customizationStructs">Массив структур.</param>
+        /// <param name="changeViewForTypeDelegate">Делегат, возвращающий представление в зависимости от типа объекта (используется когда получаемый список DataObject'ов может состоять из нескольких типов - напр. при наследовании DataObject'ов).</param>
         /// <returns>
         /// Объект <see cref="Task"/>, представляющий асинхронную операцию.
         /// Результат содержит коллекцию объектов данных.
         /// </returns>
-        Task<IEnumerable<DataObject>> LoadObjects(IEnumerable<LoadingCustomizationStruct> customizationStructs, ChangeViewForTypeDelegate changeViewForTypeDelegate);
+        Task<IEnumerable<DataObject>> LoadObjectsAsync(IEnumerable<LoadingCustomizationStruct> customizationStructs, ChangeViewForTypeDelegate changeViewForTypeDelegate);
 
         /// <summary>
         /// Загрузка объектов данных.
         /// </summary>
-        /// <param name="customizationStruct">настроичная структура для выборки<see cref="LoadingCustomizationStruct"/>.</param>
-        /// <returns>результат запроса.</returns>
+        /// <param name="customizationStruct">Настроечная структура для выборки <see cref="LoadingCustomizationStruct"/>.</param>
         /// <returns>
         /// Объект <see cref="Task"/>, представляющий асинхронную операцию.
         /// Результат содержит коллекцию объектов данных.
         /// </returns>
-        Task<IEnumerable<DataObject>> LoadObjects(LoadingCustomizationStruct customizationStruct);
+        Task<IEnumerable<DataObject>> LoadObjectsAsync(LoadingCustomizationStruct customizationStruct);
 
         /// <summary>
         /// Загрузка объектов данных.
         /// </summary>
-        /// <param name="customizationStruct">настроичная структура для выборки<see cref="LoadingCustomizationStruct"/>.</param>
-        /// <param name="State">Состояние вычитки( для последующей дочитки ).</param>
-        /// <returns></returns>
+        /// <param name="customizationStruct">Настроечная структура для выборки <see cref="LoadingCustomizationStruct"/>.</param>
+        /// <param name="State">Состояние вычитки (для последующей дочитки).</param>
         /// <returns>
         /// Объект <see cref="Task"/>, представляющий асинхронную операцию.
         /// Результат содержит коллекцию объектов данных.
         /// </returns>
-        Task<IEnumerable<DataObject>> LoadObjects(LoadingCustomizationStruct customizationStruct, ref object State);
+        //Task<IEnumerable<DataObject>> LoadObjects(LoadingCustomizationStruct customizationStruct, ref object State);
 
         /// <summary>
         /// Загрузка объектов данных.
         /// </summary>
         /// <param name="State">Состояние вычитки( для последующей дочитки).</param>
-        /// <returns></returns>
         /// <returns>
         /// Объект <see cref="Task"/>, представляющий асинхронную операцию.
         /// Результат содержит коллекцию объектов данных.
         /// </returns>
-        Task<IEnumerable<DataObject>> LoadObjects(ref object State);
+        //Task<IEnumerable<DataObject>> LoadObjects(ref object State);
 
         /// <summary>
         /// Обновление объекта данных.
         /// </summary>
-        /// <param name="dobject">объект данных, который требуется обновить.</param>
+        /// <param name="dobject">Объект данных, который требуется обновить.</param>
         /// <returns>Объект <see cref="Task"/>, представляющий асинхронную операцию.</returns>
-        Task UpdateObject(ref DataObject dobject);
+        Task<DataObject> UpdateObjectAsync(DataObject dobject);
 
         /// <summary>
         /// Обновление объектов данных.
         /// </summary>
-        /// <param name="objects">объекты данных, которые требуется обновить.</param>
+        /// <param name="objects">Объекты данных, которые требуется обновить.</param>
         /// <returns>Объект <see cref="Task"/>, представляющий асинхронную операцию.</returns>
-        Task UpdateObjects(ref IEnumerable<DataObject> objects);
+        Task<IEnumerable<DataObject>> UpdateObjectsAsync(IEnumerable<DataObject> objects);
     }
 }
---
+++ ICSSoft.STORMNET.Business/SQLDataService/SQLDataService.Async.cs
@@ -0,0 +1,102 @@
+﻿
+namespace ICSSoft.STORMNET.Business
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Data service for SQL storage.
+    /// </summary>
+    public abstract partial class SQLDataService : IAsyncDataService
+    {
+        /// <inheritdoc/>
+        public virtual async Task<int> GetObjectsCountAsync(LoadingCustomizationStruct customizationStruct)
+        {
+            return await Task.Run(() => this.GetObjectsCount(customizationStruct));
+        }
+
+        /// <inheritdoc/>
+        public virtual async Task LoadObjectAsync(DataObject dobject, bool clearDataObject = true, bool checkExistingObject = true)
+        {
+            await Task.Run(() => this.LoadObject(dobject, clearDataObject, checkExistingObject));
+        }
+
+        /// <inheritdoc/>
+        public virtual async Task LoadObjectAsync(DataObject dobject, View dataObjectView, bool clearDataObject = true, bool checkExistingObject = true)
+        {
+            await Task.Run(() => LoadObject(dataObjectView, dobject, clearDataObject, checkExistingObject));
+        }
+
+        /// <inheritdoc/>
+        public virtual async Task LoadObjectsAsync(IEnumerable<DataObject> dataobjects, View dataObjectView, bool clearDataobject = true)
+        {
+            await Task.Run(() => LoadObjects(dataobjects.ToArray(), dataObjectView, clearDataobject));
+        }
+
+        /// <inheritdoc/>
+        public virtual async Task<IEnumerable<DataObject>> LoadObjectsAsync(View dataObjectView)
+        {
+            return await Task.Run(() => LoadObjectsAsync(dataObjectView));
+        }
+
+        /// <inheritdoc/>
+        public virtual async Task<IEnumerable<DataObject>> LoadObjectsAsync(IEnumerable<View> dataObjectViews)
+        {
+            return await Task.Run(() => LoadObjects(dataObjectViews.ToArray()));
+        }
+
+        /// <inheritdoc/>
+        public virtual async Task<IEnumerable<DataObject>> LoadObjectsAsync(IEnumerable<LoadingCustomizationStruct> customizationStructs)
+        {
+            return await Task.Run(() => LoadObjects(customizationStructs.ToArray()));
+        }
+
+        /// <inheritdoc/>
+        public virtual async Task<IEnumerable<DataObject>> LoadObjectsAsync(View dataObjectView, ChangeViewForTypeDelegate changeViewForTypeDelegate)
+        {
+            return await Task.Run(() => LoadObjects(dataObjectView, changeViewForTypeDelegate));
+        }
+
+        /// <inheritdoc/>
+        public virtual async Task<IEnumerable<DataObject>> LoadObjectsAsync(IEnumerable<View> dataObjectViews, ChangeViewForTypeDelegate changeViewForTypeDelegate)
+        {
+            return await Task.Run(() => LoadObjects(dataObjectViews.ToArray(), changeViewForTypeDelegate));
+        }
+
+        /// <inheritdoc/>
+        public virtual async Task<IEnumerable<DataObject>> LoadObjectsAsync(IEnumerable<LoadingCustomizationStruct> customizationStructs, ChangeViewForTypeDelegate changeViewForTypeDelegate)
+        {
+            return await Task.Run(() => LoadObjects(customizationStructs.ToArray(), changeViewForTypeDelegate));
+        }
+
+        /// <inheritdoc/>
+        public virtual async Task<IEnumerable<DataObject>> LoadObjectsAsync(LoadingCustomizationStruct customizationStruct)
+        {
+            return await Task.Run(() => LoadObjects(customizationStruct));
+        }
+
+        /// <inheritdoc/>
+        public virtual async Task<DataObject> UpdateObjectAsync(DataObject dobject)
+        {
+            return await Task.Run(() =>
+            {
+                UpdateObject(ref dobject);
+                return dobject;
+            });
+        }
+
+        /// <inheritdoc/>
+        public virtual async Task<IEnumerable<DataObject>> UpdateObjectsAsync(IEnumerable<DataObject> objects)
+        {
+            return await Task.Run(() =>
+            {
+                var objArray = objects.ToArray();
+                UpdateObjects(ref objArray);
+                return objArray;
+            });
+        }
+    }
+}
