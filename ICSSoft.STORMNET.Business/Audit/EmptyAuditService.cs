aaca74178b666ee0b46503eeaab40dc4b750c026
tosotova <tosotova@ics.perm.ru>
2025-11-24 13:45:53 +0500

    Коррекция варианта EmptyAuditService для более стабильной работы в качестве заглушки

--- ICSSoft.STORMNET.Business/Audit/EmptyAuditService.cs
+++ ICSSoft.STORMNET.Business/Audit/EmptyAuditService.cs
@@ -1,110 +1,139 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ICSSoft.STORMNET.Business.Audit.HelpStructures;
-using ICSSoft.STORMNET.Business.Audit.Objects;
-
-namespace ICSSoft.STORMNET.Business.Audit
+﻿namespace ICSSoft.STORMNET.Business.Audit
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+
+    using ICSSoft.STORMNET.Business.Audit.HelpStructures;
+    using ICSSoft.STORMNET.Business.Audit.Objects;
+
+    /// <summary>
+    /// Сервис аудита, который не ведёт аудит.
+    /// Может использоваться в качестве заглушки.
+    /// </summary>
     public class EmptyAuditService : IAuditService
     {
+        private EmptyAudit emptyAudit = new EmptyAudit();
+
+        private AuditAppSetting auditAppSetting = new AuditAppSetting() { AuditEnabled = false, WriteSessions = false };
+
+        /// <inheritdoc/>
         public bool IsAuditEnabled => false;
 
+        /// <inheritdoc/>
         public bool IsAuditRemote => false;
 
+        /// <inheritdoc/>
         public string AuditConnectionStringName => null;
 
-        public bool ShowPrimaryKey { get => false; set => throw new NotImplementedException(); }
-        public bool PersistUtcDates { get => false; set => throw new NotImplementedException(); }
-        public AuditAppSetting AppSetting { get => null; set => throw new NotImplementedException(); }
-        public IAudit Audit { get => null; set => throw new NotImplementedException(); }
+        /// <inheritdoc/>
+        public bool ShowPrimaryKey { get => false; set { } }
 
+        /// <inheritdoc/>
+        public bool PersistUtcDates { get => false; set { } }
+
+        /// <inheritdoc/>
+        public AuditAppSetting AppSetting { get => auditAppSetting; set { } }
+
+        /// <inheritdoc/>
+        public IAudit Audit { get => emptyAudit; set { } }
+
+        /// <inheritdoc/>
         public void AddCreateAuditInformation(DataObject operationedObject)
         {
-            throw new NotImplementedException();
         }
 
+        /// <inheritdoc/>
         public void AddEditAuditInformation(DataObject operationedObject)
         {
-            throw new NotImplementedException();
         }
 
+        /// <inheritdoc/>
         public void DisableAudit()
         {
-            throw new NotImplementedException();
         }
 
+        /// <inheritdoc/>
         public bool EnableAudit(bool throwExceptions)
         {
-            throw new NotImplementedException();
+            return false;
         }
 
+        /// <inheritdoc/>
         public View GetAuditViewByType(Type type, tTypeOfAuditOperation operationType)
         {
             throw new NotImplementedException();
         }
 
+        /// <inheritdoc/>
         public View GetViewByAuditRecord(IAuditRecord auditRecord)
         {
             throw new NotImplementedException();
         }
 
+        /// <inheritdoc/>
         public bool IsTypeAuditable(Type curType)
         {
             return false;
         }
 
+        /// <inheritdoc/>
         public bool RatifyAuditOperation(tExecutionVariant executionVariant, List<Guid> auditOperationIdList, bool throwExceptions)
         {
-            throw new NotImplementedException();
+            return true;
         }
 
+        /// <inheritdoc/>
         public bool RatifyAuditOperation(tExecutionVariant executionVariant, List<Guid> auditOperationIdList, string dataServiceConnectionString, Type dataServiceType, bool throwExceptions)
         {
-            throw new NotImplementedException();
+            return true;
         }
 
+        /// <inheritdoc/>
         public bool RatifyAuditOperation(tExecutionVariant executionVariant, List<Guid> auditOperationIdList, IDataService dataService, bool throwExceptions)
         {
-            throw new NotImplementedException();
+            return true;
         }
 
+        /// <inheritdoc/>
         public bool RatifyAuditOperationWithAutoFields(tExecutionVariant executionVariant, List<AuditAdditionalInfo> auditOperationInfoList, IDataService dataService, bool throwExceptions)
         {
-            throw new NotImplementedException();
+            return true;
         }
 
+        /// <inheritdoc/>
         public Guid? WriteCommonAuditOperation(DataObject operationedObject, IDataService dataService, bool throwExceptions = true, IDbTransaction transaction = null)
         {
-            throw new NotImplementedException();
+            return Guid.NewGuid();
         }
 
+        /// <inheritdoc/>
         public void WriteCommonAuditOperationWithAutoFields(IEnumerable<DataObject> operationedObjects, ICollection<AuditAdditionalInfo> auditOperationInfoList, IDataService dataService, bool throwExceptions = true, IDbTransaction transaction = null)
         {
-            throw new NotImplementedException();
         }
 
+        /// <inheritdoc/>
         public AuditAdditionalInfo WriteCommonAuditOperationWithAutoFields(DataObject operationedObject, IDataService dataService, bool throwExceptions = true, IDbTransaction transaction = null)
         {
             throw new NotImplementedException();
         }
 
+        /// <inheritdoc/>
         public Guid? WriteCustomAuditOperation(CustomAuditParameters customAuditParameters, bool throwExceptions)
         {
-            throw new NotImplementedException();
+            return Guid.NewGuid();
         }
 
+        /// <inheritdoc/>
         public Guid? WriteCustomAuditOperation(CustomAuditParameters customAuditParameters, IDataService dataService, bool throwExceptions)
         {
-            throw new NotImplementedException();
+            return Guid.NewGuid();
         }
 
+        /// <inheritdoc/>
         public Guid? WriteCustomAuditOperation(CustomAuditParameters customAuditParameters, string dataServiceConnectionString, Type dataServiceType, bool throwExceptions)
         {
-            throw new NotImplementedException();
+            return Guid.NewGuid();
         }
     }
 }
