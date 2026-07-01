614f84bf96674dbdd7b5de34925d061606d76102
Mikhail Bobrikov <mbobrikov@neoplatform.ru>
2026-07-01 11:07:45 +0500

    добавил правильный скип тестов для mssql и oracle

--- NewPlatform.Flexberry.ORM.IntegratedTests/ICSSoft.STORMNET.Business.PostgresDataService/TimeOnlyIntegrationTest.cs
+++ NewPlatform.Flexberry.ORM.IntegratedTests/ICSSoft.STORMNET.Business.PostgresDataService/TimeOnlyIntegrationTest.cs
@@ -30,6 +30,11 @@ namespace NewPlatform.Flexberry.ORM.IntegratedTests.Postgres
 
         protected override string OracleScript => null;
 
+        /// <inheritdoc />
+        protected override void AssertWatchdog(bool notEmpty)
+        {
+        }
+
 #if NET6_0_OR_GREATER
         [Fact]
         public void TimeOnlyLinqEquals()
